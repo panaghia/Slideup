a4bb61659da866f5d51b9b2753862b7f9fe31cb5
Sergio <sergio@panaghia.it>
2011-10-14 12:05:46 +0200

    added functiona: dismiss

--- Source/Slideup.js
+++ Source/Slideup.js
@@ -9,43 +9,67 @@ authors:
 
 (function()
 {
-	Slideup = function(onEl, offEl, dialogEl, onCloseFn)
+	Slideup = function(onEl, offEl, dialogEl, onCloseFn, toolbarOffset)
 	{
 		this.onEl = document.getElementById(onEl); 
 		this.offEl = document.getElementById(offEl);
 		this.dialogEl = document.getElementById(dialogEl);
-		this.onCloseFn = onCloseFn; 
+		this.onCloseFn = onCloseFn;
+		this.toolbarOffset = toolbarOffset;
 		this.addEvents();   	
-	};
+	};     
+	
+	Slideup.prototype.getDocumentHeight = function()
+	{
+
+	    var D = document;
+	    return Math.max(
+	        Math.max(D.body.scrollHeight, D.documentElement.scrollHeight),
+	        Math.max(D.body.offsetHeight, D.documentElement.offsetHeight),
+	        Math.max(D.body.clientHeight, D.documentElement.clientHeight)
+	    );
+
+	};     
+	
+	Slideup.prototype.dismiss = function()
+	{
+		var viewportHeight = window.innerHeight;
+		this.dialogEl.style.webkitTransform = 'translate3d(0, 0px, 0)';
+		var that = this;    		
+		setTimeout(function()
+		{
+			that.dialogEl.style.display = 'none'; 
+			if(that.onCloseFn!==undefined)
+		   		that.onCloseFn.call(that);
+		}, 300);   	
+	}
 	 
 	Slideup.prototype.addEvents = function()
 	{
 		var that = this;
 		this.onEl.addEventListener('click', function(e)
 		{
-			e.preventDefault();
-			var viewportHeight = window.innerHeight;      
+			e.preventDefault(); 
+			var viewportHeight = that.getDocumentHeight();      
 		    that.dialogEl.style.top = viewportHeight+'px';
-			that.dialogEl.style.height = parseInt(viewportHeight+45)+'px';
-			that.dialogEl.style.display = 'block';
+			that.dialogEl.style.minHeight = viewportHeight+'px';
+			that.dialogEl.style.display = 'block';       
+			
 			//that.dialogEl.style.webkitTransform = 'translate3d(0, 0, 0)';
 			
 			setTimeout(function()
-			{
-				that.dialogEl.style.webkitTransform = 'translate3d(0, -'+parseInt(viewportHeight+45)+'px, 0)';
+			{                       
+				
+				if(that.toolbarOffset === undefined)                 
+					that.dialogEl.style.webkitTransform = 'translate3d(0, -'+parseInt(viewportHeight+45)+'px, 0)';
+				else
+					that.dialogEl.style.webkitTransform = 'translate3d(0, -'+parseInt(viewportHeight+that.toolbarOffset)+'px, 0)';
 			}, 100);   	   
 		}, false);  
 		this.offEl.addEventListener('click', function(e)
 		{        
 			e.preventDefault();
-			var viewportHeight = window.innerHeight;
-			that.dialogEl.style.webkitTransform = 'translate3d(0, 0px, 0)';    		
-			setTimeout(function()
-			{
-				that.dialogEl.style.display = 'none'; 
-				if(that.onCloseFn!==undefined)
-			   		that.onCloseFn.call(that);
-			}, 300);		   
+			that.dismiss();		   
 		}, false);		
 	};
 })();
